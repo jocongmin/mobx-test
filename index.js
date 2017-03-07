50448080eb6f3e9f67e90d49646977b66ea12b01
zhoucongmin <1641626308@qq.com>
2017-03-07 14:46:06 +0800

    sdlkfjsd

--- index.js
+++ index.js
@@ -24,12 +24,4 @@ class App extends React.Component {
 
 
 render( < App />, document.getElementById('app'));
-// var numbers = observable([1,2,3]);
-// var sum = computed(() => numbers.reduce((a, b) => a + b, 0));
 
-// var disposer = autorun(() => console.log(sum.get()));
-// // prints '6'
-// numbers.push(4);
-// // prints '10'
-
-// numbers.push(6);
